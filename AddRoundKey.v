5eaf9e5e387e3bc3f1ea16f527af052b631ebf9b
Hazem-Emam-404 <140505982+Hazem-Emam-404@users.noreply.github.com>
2024-04-30 17:56:08 +0300

    Add files via upload
    first upload

---
+++ AddRoundKey.v
@@ -0,0 +1,12 @@
+module AddroundKey(state,roundKey,outState);
+
+    input [127:0] state;
+    input [127:0] roundKey;
+    output  [127:0] outState;
+
+
+    assign outState = state ^ roundKey;
+
+
+
+endmodule

3d35f2a5fe229ee81db08faa1440c7e7234094a6
HossamMo123 <mah24719782471978@gmail.com>
2024-05-01 20:22:44 +0300

    mod1/4

--- inv_ShiftRow.v
+++ inv_ShiftRow.v
@@ -1,32 +1,31 @@
 module inv_shiftrow127(
-    input wire [127:0] State,
-    output reg [127:0] next 
+    input [0:127] State,
+    output [0:127] next 
 );
 
 
-always@(*)begin
-///////////////////////////first row
-next[7:0]<=State[7:0];
-next[39:32]<=State[39:32];
-next[71:64]<=State[71:64];
-next[103:96]<=State[103:96];
-////////////////////////////second row
-next[15:8]<=State[111:109];
-next[47:40]<=State[15:8];
-next[79:72]<=State[47:40];
-next[111:109]<=State[79:72];
-////////////////////////////third row
-next[23:16]<=State[87:80];
-next[55:48]<=State[119:112];
-next[87:80]<=State[23:16];
-next[119:112]<=State[55:48];
-/////////////////////////fourth row
-next[31:24]<=State[63:56];
-next[63:56]<=State[95:88];
-next[95:88]<=State[127:120];
-next[127:120]<=State[31:24];
+
+///////////////////////////
+assign next[0:7]=State[0:7];
+assign next[32:39]=State[32:39];
+assign next[64:71]=State[64:71];
+assign next[96:103]=State[96:103];
+////////////////////////////
+assign next[8:15]=State[104:111];
+assign next[40:47]=State[8:15];
+assign next[72:79]=State[40:47];
+assign next[104:111]=State[72:79];
+////////////////////////////
+assign next[16:23]=State[80:87];
+assign next[48:55]=State[112:119];
+assign next[80:87]=State[16:23];
+assign next[112:119]=State[48:55];
+/////////////////////////
+assign next[24:31]=State[56:63];
+assign next[56:63]=State[88:95];
+assign next[88:95]=State[120:127];
+assign next[120:127]=State[24:31];
 	
 
 
-end
 endmodule
